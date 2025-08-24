fa94eab656f71d5eaad69581816c078d202f776b
mrshark3959 <alfonsotheshark@gmail.com>
2025-08-24 16:23:05 -0400

    Project 1 finished
    Super simple quick project

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+from googlesearch import search
+
+
 def main():
     print(
         "Welcome to News Term Finder, please input three terms you would like to search for in the news articles."
@@ -7,15 +10,24 @@ def main():
     term2 = input("Enter second term: ")
     term3 = input("Enter third term: ")
     terms = [term1, term2, term3]
+    search_terms(terms)
 
 
 # make a function that searches for the terms
+def search_terms(terms):
+    query = " ".join(terms) + " news"
+    results = []
+    for j in search(query, num=5, stop=5, pause=2):
+        results.append(j)
+    title_printer(results)
 
 
 # make a function that prompts the user for the titles
+def title_printer(titles):
+    print("Here are the top 5 news articles found:")
+    for i, title in enumerate(titles):
+        print(f"{i + 1}. {title}")
 
-
-# make a function that sends user to the article
 
 if __name__ == "__main__":
     main()
