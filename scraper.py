890ea2071f27766e5ea64770b8655ea2685e98ed
mrshark3959 <alfonsotheshark@gmail.com>
2025-08-24 16:14:10 -0400

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,21 @@
+def main():
+    print(
+        "Welcome to News Term Finder, please input three terms you would like to search for in the news articles."
+    )
+    # take the quiries
+    term1 = input("Enter first term: ")
+    term2 = input("Enter second term: ")
+    term3 = input("Enter third term: ")
+    terms = [term1, term2, term3]
+
+
+# make a function that searches for the terms
+
+
+# make a function that prompts the user for the titles
+
+
+# make a function that sends user to the article
+
+if __name__ == "__main__":
+    main()
